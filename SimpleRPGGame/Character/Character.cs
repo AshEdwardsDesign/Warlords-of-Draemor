78422c5b76f825ef11ea64bdd46907da71d7a5b8
Ash Edwards <8330786+AshEdwardsDesign@users.noreply.github.com>
2018-12-26 22:59:15 +0000

    Initial Push
    Initial Push

---
+++ SimpleRPGGame/Character/Character.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace SimpleRPGGame
+{
+    class Character
+    {
+
+        // GENERAL INFO
+        private string firstName;
+        private string lastName;
+        private string fullName;
+        private string homeName;
+
+        // STATS
+        private int strength;
+        private int intelligence;
+        private int dexterity;
+        private int constitution;
+
+        // HEALTH & ARMOR
+        private int maxHealth;
+        private int currentHealth;
+        private decimal healthPercentage;
+        private int currentArmor;
+
+        // HEALTH PERCENTAGE CALC
+        public void calcHealthPercentage()
+        {
+            healthPercentage = ((decimal)currentHealth / (decimal)maxHealth) * 100M;
+        }
+
+        // CHARACTER CREATION
+        public void NewCharacter()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the new Character Creation Wizard!");
+
+            Console.WriteLine();
+            Console.WriteLine("Before your adventure throughout Draemor begins, you will create your character.\nThe following wizard will ask you some simple details, after which you will be shown your character sheet and your adventure can begin!");
+            Console.WriteLine();
+
+            Console.Write("What is your characters first name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("What is your characters last name: ");
+            lastName = Console.ReadLine();
+
+            fullName = string.Format($"{firstName} {lastName}");
+
+            Console.Write("What is the name of your characters hometown: ");
+            homeName = Console.ReadLine();
+
+            strength = 1;
+            intelligence = 1;
+            dexterity = 1;
+            constitution = 1;
+
+            maxHealth = 91;
+            currentHealth = 42;
+            calcHealthPercentage();
+            currentArmor = 0;
+
+            Console.WriteLine();
+            Console.WriteLine("Character creation completed!");
+            Console.WriteLine("Press enter to continue and view your character sheet.");
+            Console.ReadLine();
+            DisplayCharacterSheet();
+        }
+
+        // DISPLAY CHARACTER SHEET
+        public void DisplayCharacterSheet()
+        {
+            Console.Clear();
+            Console.WriteLine("YOUR CHARACTER SHEET");
+            Console.WriteLine();
+            Console.WriteLine("// CHARACTER BIO //");
+            Console.WriteLine();
+            Console.WriteLine($"Character Name: {fullName}");
+            Console.WriteLine($"Character hails from: {homeName}");
+            Console.WriteLine();
+            Console.WriteLine("// CHARACTER STATS //");
+            Console.WriteLine();
+            Console.WriteLine($"Strength: {strength}");
+            Console.WriteLine($"Intelligence: {intelligence}");
+            Console.WriteLine($"Dexterity: {dexterity}");
+            Console.WriteLine($"Constitution: {constitution}");
+            Console.WriteLine();
+            Console.WriteLine("// CURRENT STATUS //");
+            Console.WriteLine();
+            Console.WriteLine($"Current/Max Health: {currentHealth}/{maxHealth} ({healthPercentage.ToString("F2")}%)");
+            Console.WriteLine($"Current Armor rating: {currentArmor}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue.");
+        }
+
+
+    }
+}
---
+++ SimpleRPGGame/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SimpleRPGGame
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool replay = true;
+
+            while (replay)
+                replay = MainMenu(); 
+
+        }
+
+        static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("WARLORDS OF DRAEMOR");
+            Console.WriteLine("Welcome to Warlords of Draemor; A text-based fantasy RPG, developed by Ash Edwards");
+
+            Console.WriteLine();
+            Console.WriteLine("Main Menu:");
+            Console.WriteLine();
+
+            Console.WriteLine("1. New Game");
+            Console.WriteLine("2. Encyclopedia");
+            Console.WriteLine("3. About");
+            Console.WriteLine("4. Quit");
+
+            Console.WriteLine();
+            Console.Write("Please choose a number: ");
+            string mainMenuChoice = Console.ReadLine();
+
+            switch (mainMenuChoice)
+            {
+                case "1":
+                    Character player = new Character();
+                    player.NewCharacter();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("The game will now exit. Thank you for playing.");
+                    Console.ReadLine();
+                    return false;
+                default:
+                    Console.WriteLine("Sorry, I didn't recognise that option!");
+                    break;
+            }
+
+            Console.ReadLine();
+            return true;
+        }
+    }
+}
